f0bdb61df4d9a01877a14f57682d85e9dd38814e
mrFredBar <danil.pischaschyi@gmail.com>
2025-07-13 08:31:04 +0200

    Update nr 3.2.py
    Змінив перевірку данних, також відображення помилки при вводі через ValueError

--- nr 3.2.py
+++ nr 3.2.py
@@ -1,31 +1,52 @@
 #nr 3.2
 import random
 
-quantity = int(input("Введите количество чисел в лотерее: "))
-min = int(input("Введите минимальное число лотереи, не меньше 1: "))
-max = int(input("Введите максимальное число лотереи, не больше 1000: "))
-
-#Проверка значений 
-if quantity <0:
-    print ('Количество чисел должно быть больше 0')
-if min < 1 :
-    print('Минимальное число не выполняет требование "не меньше 1"')
-    exit()
-if max > 1000:
-    print('Максимальное число не выполняет требование "не больше 1000"')
-    exit()
-if min >= max:
-    print ('Минималное число не может быть больше или равно максимальному')
-    exit()
-
 #Написать функцию 'get_numbers_ticket(min, max, quantity)' которая будет генерировать набор уникальных чисел для лотереи
 def get_numbers_ticket(min, max, quantity):
+    
     numbers = []
+    #Проверка значений 
+    if quantity <=0:
+       #print ('Количество чисел должно быть больше 0')
+       return[]
+       
+    if min < 1 :
+       #print('Минимальное число не выполняет требование "не меньше 1"')
+       return[]
+    
+    if max > 1000:
+       #print('Максимальное число не выполняет требование "не больше 1000"')
+       return[]
+
+    if min >= max:
+       #print ('Минималное число не может быть больше или равно максимальному')
+       return[]
+
+    if quantity > (max - min + 1):
+       #print ('Ошибка ввода данных. Количество чисел больше диапазона')
+       return[]
+    
+
     while len(numbers) < quantity:
         num = random.randint(min, max)
         if num not in numbers:
             numbers.append(num)
     return sorted(numbers)
 
+#Ввод данных
+try:
+   quantity_int = int(input("Введите количество чисел в лотерее: "))
+   min_int = int(input("Введите минимальное число лотереи, не меньше 1: "))
+   max_int = int(input("Введите максимальное число лотереи, не больше 1000: "))
+
+#Проверка ввода данных на отрицательные значения (просьба правки после сдачи)
+   if quantity_int < 0 or min_int < 0 or max_int < 0:
+      #print ('Ошибка ввода данных, введите ТОЛЬКО положительные числа')
+      results = []
+   else:
+       results = get_numbers_ticket(min_int, max_int, quantity_int)
+except ValueError:
+   results = []
+
 #Вывод результата
-print ('Результаты лотереи:', get_numbers_ticket(min, max, quantity))
+print ('Результаты лотереи:', results)
